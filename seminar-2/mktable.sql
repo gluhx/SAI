43caa96869faad19f4eb78bd128cd069b247a8ae
gluhx <stepa.gl@yandex.ru>
2025-09-14 16:17:31 +0300

    Begining seminar-2

---
+++ seminar-2/mktable.sql
@@ -0,0 +1,11 @@
+SPOOL ~/Desktop/SAI/seminar-2/mkTable.list
+
+PROMPT Создание таблиц
+
+PROMPT Создание таблицы слотов "slots"
+
+DROP TABLE slots CASCADE CONSTRAINTS;
+CREATE TABLE slots (
+    slot_id     NUMBER,
+
+);
